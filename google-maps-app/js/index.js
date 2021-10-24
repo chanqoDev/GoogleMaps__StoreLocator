c878d07d9ab445b1ae8806f2245913dafb22c495
Christian E <christianelena92@gmail.com>
2021-10-23 19:46:44 -0500

    import Google Maps API keys too

--- google-maps-app/js/index.js
+++ google-maps-app/js/index.js
@@ -1,1 +1,9 @@
- 
+let map;
+
+function initMap() {
+  const losAngeles = { lat: 34.06338, lng: -118.35808 };
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: losAngeles,
+    zoom: 8,
+  });
+}
