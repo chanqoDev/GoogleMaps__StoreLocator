4a03d3c46876f81bf630ab13a7a8faeef774f9bc
Christian E <christianelena92@gmail.com>
2021-10-24 17:45:36 -0500

    Create a Store model to define the schema of the data

--- google-maps-app-api/app.js
+++ google-maps-app-api/app.js
@@ -1,13 +1,31 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
+const Store = require("./api/models/store");
 
+mongoose.connect(
+  "mongodb+srv://chanqo_dev:pCeo4OvKIAAbOwmW@cluster0.3njeu.mongodb.net/cluster0?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 // body parser middleware has been deprecated!
 app.use(express.json({ limit: "50mb" }));
 
 app.post("/api/stores", (req, res) => {
   let dbStores = req.body;
   console.log(dbStores);
+  var store = new Store({
+    storeName: "Test",
+    phoneNumber: "9493840248",
+    location: {
+      type: "Point",
+      coordinates: [-118.376354, 34.063584],
+    },
+  });
+  store.save();
   res.send("You have posted");
 });
 
