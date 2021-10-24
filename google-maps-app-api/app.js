46ab91c798c75274dc2f32e2c972f3c1714911e6
Christian E <christianelena92@gmail.com>
2021-10-24 16:30:16 -0500

    set up api stores endpoint

--- google-maps-app-api/app.js
+++ google-maps-app-api/app.js
@@ -2,9 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// body parser middleware has been deprecated!
+app.use(express.json({ limit: "50mb" }));
+
+app.post("/api/stores", (req, res) => {
+  let dbStores = req.body;
+  console.log(dbStores);
+  res.send("You have posted");
 });
+
+app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
