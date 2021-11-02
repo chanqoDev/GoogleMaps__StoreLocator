d6c14fc7504803075c419c06d443eef197191b71
Christian E <christianelena92@gmail.com>
2021-11-02 14:23:15 -0500

    sanitize files and styling

--- google-maps-app/js/index.js
+++ google-maps-app/js/index.js
@@ -1,34 +1,19 @@
 let map;
+let markers = [];
 let infoWindow;
-var markers = [];
+let locationSelect;
+const API_URL = "http://localhost:3001/api/stores";
 
-function initMap() {
-  const losAngeles = {
-    lat: 34.06338,
-    lng: -118.35808,
-  };
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: losAngeles,
-    zoom: 11,
-  });
-
-  infoWindow = new google.maps.InfoWindow();
-}
 const onEnter = (e) => {
-  if (e.key === "enter") {
+  if (e.key == "Enter") {
     getStores();
   }
 };
 
 const getStores = () => {
-  const zipCode = document.getElementById("zip-code").value;
-  if (!zipCode) {
-    return;
-  }
-
-  const API_URL = "http://localhost:3000/api/stores";
-  const fullUrl = `${API_URL}?zipCode=${zipCode}`;
-  fetch(API_URL)
+  const zipCode = document.getElementById("zip-code-input").value;
+  const fullUrl = `${API_URL}?zip_code=${zipCode}`;
+  fetch(fullUrl)
     .then((response) => {
       if (response.status == 200) {
         return response.json();
@@ -37,134 +22,151 @@ const getStores = () => {
       }
     })
     .then((data) => {
-      if (data.length > 0) {
-        clearLocation();
-        setStoresList(data);
-        searchLocationsNear(data);
-        setOnClickListener();
-      } else {
-        clearLocation();
-        noStoresFound();
-      }
+      setData(data);
     });
 };
 
-const clearLocation = () => {
+const setData = (data) => {
+  if (data.length == 0) {
+    clearLocations();
+    noStoresFound();
+  } else {
+    setStoresList(data);
+    searchLocationsNear(data);
+    setOnClickListener();
+  }
+};
+
+initMap = () => {
+  var losAngeles = { lat: 34.06338, lng: -118.35808 };
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: losAngeles,
+    zoom: 11,
+    mapTypeId: "roadmap",
+    mapTypeControl: false,
+    fullscreenControl: false,
+    streetViewControl: false,
+  });
+  infoWindow = new google.maps.InfoWindow();
+};
+
+const noStoresFound = () => {
+  const html = `
+    <div class="no-stores-found">
+        No Stores Found
+    </div>
+    `;
+  document.querySelector(".stores-list").innerHTML = html;
+};
+
+function setOnClickListener() {
+  var storeElements = document.querySelectorAll(".store-container");
+  storeElements.forEach((elem, index) => {
+    elem.addEventListener("mouseover", () => {
+      elem
+        .querySelector(".store-number")
+        .classList.add("store-number-hover-state");
+    });
+    elem.addEventListener("mouseout", () => {
+      elem
+        .querySelector(".store-number")
+        .classList.remove("store-number-hover-state");
+    });
+    elem.addEventListener("click", function () {
+      new google.maps.event.trigger(markers[index], "click");
+    });
+  });
+}
+
+const setStoresList = (stores) => {
+  let storesHtml = "";
+  stores.map((store, index) => {
+    storesHtml += `
+        <div class="store-container">
+            <div class="store-container-background">
+                <div class="store-info-container">
+                    <div class="store-address">
+                        <span>${store["addressLines"][0]}</span>
+                        <span>${store["addressLines"][1]}</span>
+                    </div>
+                    <div class="store-phone-number">${
+                      store["phoneNumber"]
+                    }</div>
+                </div>
+                <div class="store-number-container">
+                    <div class="store-number">
+                        ${index + 1}
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+  });
+  document.querySelector(".stores-list").innerHTML = storesHtml;
+};
+
+const clearLocations = () => {
   infoWindow.close();
-  for (let i = 0; i < markers.length; i++) {
+  for (var i = 0; i < markers.length; i++) {
     markers[i].setMap(null);
   }
   markers.length = 0;
 };
 
-const noStoresFound = () => {
-  const html = `
-  <div class="no-stores-found">
-    <h2>No stores found</h2>
-  </div>
-  `;
-  document.querySelector(".stores-list").innerHTML = html;
-};
-
-const setOnClickListener = () => {
-  let storeElements = document.querySelectorAll(".store-container");
-  storeElements.forEach((elem, index) => {
-    elem.addEventListener("click", () => {
-      google.maps.event.trigger(markers[index], "click");
-    });
-  });
-};
-
-const setStoresList = (stores) => {
-  let storesHtml = "";
-  stores.forEach((store, index) => {
-    storesHtml += `
-    <div class="store-container">
-    <div class="store-container-background">
-        <div class="store-info-container">
-            <div class="store-address">
-                <span class="address-line1">${
-                  store.addressLines[0]
-                }</span>                     
-                <span class="address-line2">${store.addressLines[1]}</span>
-            </div>
-            <div class="store-phone-number">
-                ${store.phoneNumber}
-            </div>
-            <div class="store-number-container">
-                <div class="store-number">
-                   ${index + 1}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-    `;
-  });
-  document.querySelector(".stores-list").innerHTML = storesHtml;
-};
 const searchLocationsNear = (stores) => {
+  clearLocations();
   var bounds = new google.maps.LatLngBounds();
-
-  stores.forEach((store, index) => {
-    let latlng = new google.maps.LatLng(
-      store.location.coordinates[1],
-      store.location.coordinates[0]
+  stores.map((store, index) => {
+    var latlng = new google.maps.LatLng(
+      parseFloat(store.location["coordinates"][1]),
+      parseFloat(store.location["coordinates"][0])
     );
-    let name = store.storeName;
-    let address = store.addressLines[0];
-    let phone = store.phoneNumber;
-    let openStatusText = store.openStatusText;
+    let name = store["storeName"];
+    let address = store["addressLines"][0];
+    let fullAddress = `${store["addressLines"][0]} ${store["addressLines"][1]}`;
     bounds.extend(latlng);
-    createMaker(latlng, name, address, openStatusText, phone, index + 1);
+    createMarker(latlng, name, address, fullAddress, index + 1);
   });
   map.fitBounds(bounds);
 };
 
-const createMaker = (
-  latlng,
-  name,
-  address,
-  openStatusText,
-  phone,
-  storeNumber
-) => {
-  let html = `
-  <div class="store-info-window">
-  <div class="store-info-name">
-      ${name}
-  </div>
-    <div class="store-info-open-status">
-      ${openStatusText}
-    </div>
-    <div class="store-info-address">
-      <div class="icon"> 
-        <i class="fas fa-location-arrow"></i>
-        </div> 
-        <span>${address}</span>
-    </div> 
-  
-   <div class="store-info-phone">
-   <div class="icon">
-     <i class="fas fa-phone-alt"></i>
-   </div> 
-   <span>
-    <a href="tel:${phone}">${phone}</span> 
-   </div>
-  </div> 
-  `;
-  const infowindow = new google.maps.InfoWindow({
-    content: html,
-  });
+const createMarker = (latlng, name, address, fullAddress, label) => {
+  let googleUrl = new URL("https://www.google.com/maps/dir/");
+  googleUrl.searchParams.append("api", "1");
+  googleUrl.searchParams.append("destination", fullAddress);
 
-  let marker = new google.maps.Marker({
+  const html = `
+        <div class="store-info-window">
+            <div class="store-info-name">
+                ${name}
+            </div>
+            <div class="store-info-open-status">
+                Open until 6:00 PM
+            </div>
+            <div class="store-info-address">
+                <div class="icon">
+                    <i class="fas fa-location-arrow"></i>
+                </div>
+                <span>
+                    <a target="_blank" href="${googleUrl.href}">${address}</a>
+                </span>
+            </div>
+            <div class="store-info-phone">
+                <div class="icon">
+                    <i class="fas fa-phone-alt"></i>
+                </div>
+                <span><a href="tel:310-659-9562">310-659-9562</a></span>
+            </div>
+        </div>
+    `;
+  var marker = new google.maps.Marker({
     map: map,
     position: latlng,
-    label: `${storeNumber}`,
+    label: label.toString(),
   });
   google.maps.event.addListener(marker, "click", function () {
-    infowindow.setContent(html);
-    infowindow.open(map, marker);
+    infoWindow.setContent(html);
+    infoWindow.open(map, marker);
   });
   markers.push(marker);
 };
